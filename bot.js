b015cd845aaf7add80829e43a882d1adf272b781
Kathryn Beaty <kabeaty@gmail.com>
2014-12-22 14:17:40 -0500

    Add traverson and superagent; add ability to get Artsy token and make Artsy api calls

--- bot.js
+++ bot.js
@@ -1,5 +1,43 @@
+var request = require('request');
 // Twitter library
 var Twit = require('twit');
+
+// for Artsy requests
+var request = require('superagent');
+var traverson = require('traverson');
+var api = traverson.jsonHal.from('https://api.artsy.net/api/');
+var clientID = '80f24fbda1ca089c92e8',
+    clientSecret = 'd26c0ef2d239f4e2d2e178fb5866b58a',
+    apiUrl = 'https://api.artsy.net/api/tokens/xapp_token',
+    xappToken;
+
+function getToken() {
+  request
+  .post(apiUrl)
+  .send({ client_id: clientID, client_secret: clientSecret })
+  .end(function(res) {
+    xappToken = res.body.token;
+    getArtists();
+  });
+}
+
+
+function getArtists() {
+  api.newRequest()
+  .follow('artists')
+  .withRequestOptions({
+    headers: {
+      'X-Xapp-Token': xappToken,
+      'Accept': 'application/vnd.artsy-v2+json'
+    }
+  })
+  .getResource(function(error, res) {
+    console.log(error);
+    console.log(res.);
+  });
+}
+
+getToken();
 
 // Include config file
 var T = new Twit(require('./config.js'));
